8bc5f8f5a7800126798f51535891f8a3b03551fd
kpcyrd <git@rxv.cc>
2023-01-20 02:13:33 +0100

    Fix help text

--- src/args.rs
+++ src/args.rs
@@ -19,16 +19,16 @@ pub struct Args {
     /// Always check for updates when starting
     #[arg(long)]
     pub check_update: bool,
-    /// Always check for updates when starting
+    /// Never check for updates when starting
     #[arg(long)]
     pub skip_update: bool,
     /// Update even if latest version is already installed
     #[arg(long)]
     pub force_update: bool,
-    /// Run the install/update code but don't actually run the final binary
+    /// Check for the latest .deb and print its url
     #[arg(long)]
     pub print_deb_url: bool,
-    /// Check for the latest .deb and print its url
+    /// Run the install/update code but don't actually run the final binary
     #[arg(long)]
     pub no_exec: bool,
     /// The timeout to use for http connections and requests
