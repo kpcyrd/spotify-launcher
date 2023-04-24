e0ed613302bc3d7fe75a61628b90ae48c566792d
Magnus Groß <magnus@mggross.com>
2023-04-24 21:37:43 +0200

    Check URI explicitly for supported schemes
    This is just a precautionary check so that we never accidentally send an
    unsupported URI over dbus.
    
    We capture the type and id of the URI in capture groups, so that we can
    construct the correct URI directly.

--- src/dbus.rs
+++ src/dbus.rs
@@ -18,11 +18,36 @@ pub fn is_spotify_running() -> bool {
     }
 }
 
+fn parse_uri(uri: &String) -> Result<String> {
+    // Remove useless tracking IDs
+    let sanitized_uri = Regex::new(r"\?.*").unwrap().replace_all(uri, "");
+
+    // URIs may be of one of the following forms:
+    // 1. spotify:type:id
+    // 2. spotify://type/id
+    //
+    // Here type must be something like "track" or "album"
+    // and id is a unique id consisting of alphanumerical symbols.
+    //
+    // The dbus interface only understands the first syntax,
+    // so we have to transform the second scheme into the first one.
+    let r = Regex::new(r"^spotify:(?://)?(?P<type>[a-z]+)(?::|/)(?P<id>[[:alnum:]]+)$").unwrap();
+    match r.captures(&sanitized_uri) {
+        Some(c) => Ok(format!(
+            "spotify:{}:{}",
+            c.name("type").unwrap().as_str(),
+            c.name("id").unwrap().as_str()
+        )),
+        None => Err(zbus::Error::Failure(
+            "Unsupported URI scheme for dbus".to_string(),
+        )),
+    }
+}
+
 pub fn play_remote(uri: &String) -> Result<Arc<zbus::Message>> {
     info!("Playing uri {uri} in already running instance over dbus");
     let c = zbus::blocking::Connection::session()?;
-    // Remove useless tracking IDs
-    let sanitized_uri = Regex::new(r"\?.*").unwrap().replace_all(uri, "");
+    let parsed = parse_uri(uri)?;
     // This dbus interface supports URIs in the spotify:<type>:<UUID> format,
     // which conveniently is also the format used by the "--uri" arg
     c.call_method(
@@ -30,6 +55,6 @@ pub fn play_remote(uri: &String) -> Result<Arc<zbus::Message>> {
         "/org/mpris/MediaPlayer2",
         Some("org.mpris.MediaPlayer2.Player"),
         "OpenUri",
-        &(sanitized_uri),
+        &(parsed),
     )
 }
